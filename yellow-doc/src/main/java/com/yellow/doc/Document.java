75790a42fd63746e8d1868aae494c3348cd0a182
huangwei <huangwei@fanggeek.com>
2019-02-20 17:40:53 +0800

    更新 document 内容

--- yellow-doc/src/main/java/com/yellow/doc/Document.java
+++ yellow-doc/src/main/java/com/yellow/doc/Document.java
@@ -4,9 +4,32 @@ import com.yellow.anno.MyId;
 
 public class Document {
 
-	@MyId
-	private String name;
-	
-	@MyId
-	private Integer age;
+    @MyId
+    private String name;
+    
+    @MyId
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Document [name=" + name + ", age=" + age + "]";
+    }
+    
+    
 }
